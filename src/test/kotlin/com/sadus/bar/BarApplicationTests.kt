9671bb32127c11009887c7a766d0c90b52af7b0d
sbebenzi <sarahlover822@gmail.com>
2025-05-04 23:02:35 -0300

    first commit initializing project!

---
+++ src/test/kotlin/com/sadus/bar/BarApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sadus.bar
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BarApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
